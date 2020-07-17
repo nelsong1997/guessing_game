dc5732a940f680d775a17e14aedd76c992ce747d
Gabe N <nelsong1997@gmail.com>
2020-07-17 14:20:36 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            theWords: null
+        }
+        this.chooseWords = this.chooseWords.bind(this);
+    }
+
+    componentDidMount() {
+        this.chooseWords()
+    }
+
+    chooseWords() {
+        let wordArray = []
+        for (let i=0; i<25; i++) wordArray[i] = "word" + i
+        this.setState({theWords: wordArray})
+    }
+
+    displayCards() {
+        if (!this.state.theWords) return
+        let theRows = []
+        let theColumns = []
+        for (let i=0; i<5; i++) {
+            theColumns[i] = []
+            for (let j=0; j<5; j++) {
+                theColumns[i][j] = (
+                    <div id={"box-" + (5*i + j)} key={5*i + j} className="box">
+                        {this.state.theWords[5*i + j]}
+                    </div>
+                )
+            }
+            theRows[i] = <div id={"row-" + i} key={i} className="row">{theColumns[i]}</div>
+        }
+        return(
+            <div id="the-boxes">
+                {theRows}
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div id="main">
+                {this.displayCards()}
+            </div>
+        )
+    }
 }
 
 export default App;
